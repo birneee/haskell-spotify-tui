9408bc62e206c1943fa4b747280af7c8c3fd0501
benedikt <benedikt.spies@gmail.com>
2020-08-29 16:45:09 +0200

    Merged

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
-import           CLI (cliMain)
+import qualified Controller as CONTROLLER (play)
+import Tui.Tui as TUI (app, playAttr, stopAttr, nextAttr, previousAttr)
+import AppState (AppState, execAppStateIO, newAppState)
 
-main :: IO ()
-main = cliMain
+import Brick
+  (App(..), AttrMap, BrickEvent(..), EventM, Next, Widget
+  , defaultMain
+  )
+
+main :: IO AppState
+main = do
+       defaultMain app newAppState
