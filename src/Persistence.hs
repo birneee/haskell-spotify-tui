4433a8e051e05fbf7dabee3c94e15c543a11c23e
leo-mao <emicool.my@gmail.com>
2020-09-08 15:35:56 +0200

    something

--- src/Persistence.hs
+++ src/Persistence.hs
@@ -1,25 +1,83 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
 module Persistence where
 
-import           ApiObjects.AccessToken  (AccessToken)
-import           ApiObjects.RefreshToken (RefreshToken)
-import           Authenticator           (getAccessToken, getAuthorizationCode,
-                                          getRefreshToken)
+import Control.Lens (makeLenses, (^.), (.~), (&))
+
+import           ApiObjects.RefreshToken  (RefreshToken) 
+
 import           Utils.StringUtils
+
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import qualified Data.HashMap.Strict as HM
+
+data ConfigItem = 
+    ConfigItem {
+        _clientId :: Maybe String -- um die Lens unterscheiden
+      , _clientSecret :: Maybe String
+      , _refreshToken :: Maybe RefreshToken} deriving(Show, Eq)
+
+$(makeLenses ''ConfigItem)
+
+instance FromJSON ConfigItem where
+    parseJSON (Object v) =  ConfigItem
+        <$> v .: "clientId"
+        <*> v .: "clientSecret" 
+        <*> (pack  <$> v .: "refreshToken")
+
+instance ToJSON ConfigItem where
+    toJSON (ConfigItem clientId' clientSecret' refreshToken') =
+        object ["clientId" .= clientId'
+        , "clientSecret" .= clientSecret'
+        , "refreshToken" .= unpack refreshToken']
+    toEncoding (ConfigItem clientId' clientSecret' refreshToken') =
+        pairs ("clientId" .= clientId'
+        <> "clientSecret" .= clientSecret'
+        <> "refreshToken" .= unpack refreshToken')
+
+configFile :: FilePath
+-- configFile = "config.json"
+configFile = "empty"
+
+saveConfig :: ConfigItem -> IO ()
+saveConfig config = B.writeFile configFile (encode config)
+    
+loadConfig :: IO ConfigItem
+loadConfig = do 
+    input <- B.readFile configFile
+    return (decode input) ?: emptyConfig
+
+emptyConfig :: ConfigItem
+emptyConfig = ConfigItem Nothing Nothing Nothing
+
+-- update :: RefreshToken -> ConfigItem -> RefreshToken -> Value
+-- update refreshToken content = 
+--   Object $ HM.insert "refreshToken" refreshToken content
 
 saveRefreshToken :: RefreshToken -> IO ()
 -- |Save refresh token in file
 -- |TODO use config.json file
-saveRefreshToken refreshToken = do
-  let filePath = "refresh_token.tmp"
-  writeFile filePath $ unpack refreshToken
+saveRefreshToken refreshToken' = do
+  content <- loadConfig
+  undefined
+  -- let config' = case content of
+  --     Nothing -> content 
+  --     Just config -> content & refreshToken .~ refreshToken'
+  --     update refreshToken content
+
+  --     saveConfig config
+
 
 loadRefreshToken :: IO (Maybe RefreshToken)
 -- |Get refresh token from file "refresh_token.tmp"
 -- |TODO load from persistent config.json file
 loadRefreshToken = do
-  let filePath = "refresh_token.tmp"
-  appendFile filePath ""
-  content <- readFile filePath
-  case (length content) of
-    0 -> return Nothing
-    _ -> return $ Just $ pack content
+  content <- loadConfig
+  case content of
+    Nothing -> return Nothing
+    Just config -> return $ Just $ config ^. refreshToken
+  -- return $ Just $ _refreshToken content
+  -- let r' =  $ Just content ^. refreshToken
+  -- return r' 
